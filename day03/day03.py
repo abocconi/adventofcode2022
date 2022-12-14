d7be5b95fff807723500060189054342d6e3acf5
abocconi <andrea.bocconi@gmail.com>
2022-12-14 23:44:46 +0100

    day03-2 done

--- day03/day03.py
+++ day03/day03.py
@@ -1,17 +1,27 @@
+def item_priority_get(item : str):
+    if item.islower():
+        return ord(item) + 1 - ord('a')
+    else:
+        return ord(item) + 27 - ord('A')
+   
 
-def priority_get(rucksack : str):
-    rucksack  = rucksack.rstrip()    
-    s1 = set(rucksack[0:int(len(rucksack)/2)])    
+def rucksack_priority_get(rucksack : str):
+    rucksack  = rucksack.rstrip()
+    s1 = set(rucksack[0:int(len(rucksack)/2)])
     s2 = set(rucksack[int(len(rucksack)/2):])
-    retval = sum((ord(c) + 1 - ord('a')) for c in s1.intersection(s2) if ord(c) >= ord('a'))
-    retval += sum((ord(c) + 27 - ord('A')) for c in s1.intersection(s2) if ord(c) < ord('a'))
-    print(s1.intersection(s2), retval)
-    return retval
+    return sum(item_priority_get(item) for item in s1.intersection(s2))
 
+def group_priority_get(rucksacks : list[str]):
+    s1 = set(rucksacks[0].rstrip()).intersection(set(rucksacks[1].rstrip()))
+    s2 = set(rucksacks[2].rstrip())
+    return sum(item_priority_get(item) for item in s1.intersection(s2))
 
 with open("input.txt") as f:
-    total = sum(priority_get(line) for line in f.readlines())
+    rucksacks = f.readlines()
+    # part 1
+    print("total (1):", sum(rucksack_priority_get(line) for line in rucksacks))
+    # part 2
+    print("total (2):", sum(group_priority_get(rucksacks[i:i+3]) for i in range(0, len(rucksacks), 3)))
     
-print(total)
-    
+   
     
