e9e863569f48bc265ce2e1c9bdb68befe4225d75
abocconi <andrea.bocconi@gmail.com>
2022-12-25 23:00:08 +0100

    day06 done

---
+++ day06/day06.py
@@ -0,0 +1,20 @@
+
+with open("input.txt") as f:
+    stream = f.read()    
+
+#part 1    
+index = 0
+found = False
+while len(set(stream[index:index+4])) != 4:
+    index += 1
+
+print(stream[index:index+4], index+4)
+
+#part 2
+index = 0
+found = False
+while len(set(stream[index:index+14])) != 14:
+    index += 1
+
+print(stream[index:index+4], index+14)
+
